10ee30f1675e92c3305d527c006691aed24a99c0
Duong Le <tduongle10@gmail.com>
2026-07-23 20:53:04 +0700

    set up project

---
+++ LittleLemon/LittleLemon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LittleLemon
+//
+//  Created by Duong Le on 7/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LittleLemon/LittleLemon/LittleLemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonApp.swift
+//  LittleLemon
+//
+//  Created by Duong Le on 7/23/26.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
